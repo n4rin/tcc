5cdb8950f0167ddfefe346bbd95827b8d2e16f4e
Aline Soares <99834657+n4rin@users.noreply.github.com>
2023-11-02 23:45:08 -0300

    pequenos ajustes
    pequenas alterações em produtos, arrumando o css e php

--- Pagina/chas.php
+++ Pagina/chas.php
@@ -205,51 +205,8 @@ function searchProductsByTag($tag)
         ?>
     </div>
 
-
     <!--Footer-->
-    <footer>
-        <div id="footer-content">
-            <div id="footer-contacts">
-                <h2>LIVATY</h2>
-                <p>Nos siga nas redes sociais</p>
-
-                <div id="footer-social-media">
-                    <a href="#" class="footer-link" id="instagram"><i class='bx bxl-instagram'></i></a>
-                    <a href="#" class="footer-link" id="facebook"><i class='bx bxl-facebook-square'></i></a>
-                    <a href="#" class="footer-link" id="whatsapp"><i class='bx bxl-whatsapp'></i></a>
-                </div>
-            </div>
-
-            <ul class="footer-list">
-                <li>
-                    <h3>Atendimento</h3>
-                </li>
-                <li><a href="#" class="footer-link">Formas de Pagamento</a></li>
-                <li><a href="#" class="footer-link">Catálogo</a></li>
-                <li><a href="#" class="footer-link">Institucional</a></li>
-            </ul>
-
-            <ul class="footer-list">
-                <li>
-                    <h3>Produtos</h3>
-                </li>
-                <li><a href="#" class="footer-link">Cafés</a></li>
-                <li><a href="#" class="footer-link">Chás</a></li>
-                <li><a href="#" class="footer-link">Incensos</a></li>
-            </ul>
-
-            <div id="footer-map">
-                <h3>Nossa sede:</h3>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3684.4530145073554!2d-47.42621522712088!3d-22.562154025640524!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c8806c2c324933%3A0xe2108428886c8646!2sCol%C3%A9gio%20T%C3%A9cnico%20de%20Limeira%20-%20Unicamp!5e0!3m2!1spt-BR!2sbr!4v1696366241972!5m2!1spt-BR!2sbr" width="300" height="200" style="border-radius:15px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-            </div>
-        </div>
-        <div id="footer-copyright">
-            &#169
-            2023 all rights reserved
-        </div>
-
-    </footer>
-
+    <?php include 'footer.php'; ?>
 
     <script src="styles.js"></script>
     <script src="carrinho.js"></script>
---
+++ Produtos/telaProduto1.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="icon" type="image/x-icon" href="../imagem/logo.PNG">
+    <link rel="stylesheet" href="produto.css">
+    <link rel="stylesheet" href="../Pagina/style.css">
+    <title>LivaTy - Chá Matte Leão</title>
+</head>
+
+<body style="background-color: #f7f3f2; color: #27211E;">
+    <!--Inicio NavBar-->
+    <?php include 'navbar2.php'; ?>
+
+
+    <br>
+    <div style="height: 90px;"></div>
+    <div id="achubaba" class="container border border-secondary-subtle row"
+        style="  display: flex; width: 980px; height: 722px; margin: auto; border-radius: 20px;"><br>
+        <div id="achubaba" style="width: 890px; height: 671.19px; margin: auto;">
+            <section style="left: 100%; width: 500px; height: 671.1px;">
+                <div style="width: 500px; height: 500px;">
+                    <img src="../imagem/1.png" alt="cafe"
+                        style="width: 500px; height: 500px; border-radius: 10px;">
+                </div>
+            </section>
+            <div style="width: 35px; height: 671.19px;"></div>
+            <section class="secaoProdutos" style="width: 355px; height: 671.19px;">
+                <div style="width: 355px; height: 243.19px;">
+                    <div style="margin: auto; height: 50px;">
+                        <h4>Chá Matte Leão - 250g</h4>
+                    </div>
+                    <div style="height: 40px;">
+                        <h5>R$: 12,90</h5>
+                    </div>
+                    <div style=" height: 40px;">
+                        <div>
+                            <p>Quantidade</p>
+                            <input type="number" max="100" min="1"
+                                style="width: 80px; height: 40px; background-color: #f7f3f2; text-align: center; border-radius: 2px; border-color: #27211E;">
+                        </div>
+                        <div style="height: 20px;"></div>
+                        <div>
+                            <button class="btn-produtc"><a href="../Pagina/telaCarrinho.html">Adicionar ao Carrinho</a></button>
+                        </div><br>
+                        <h5>Descrição do produto. ex.: quantas gramas, quantas unidades, especificações especiais,etc.</h5>
+                    </div>
+
+                </div>
+            </section>
+        </div>
+
+    </div><br>
+
+    <!--Footer-->
+    <?php include '../Pagina/footer.php'; ?>
+
+    <script src="../Pagina/styles.js"></script>
+    <script src="../Pagina/carrinho.js"></script>
+</body>
+
+</html>
