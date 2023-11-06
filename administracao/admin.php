a9459ff2888a010c26ac0e11706873c25100b4d4
Vinícius Miguel <99834882+vinicius-magi@users.noreply.github.com>
2023-11-06 16:10:56 -0300

    Adicionar/deletar produtos - funcionando

--- administracao/admin.php
+++ administracao/admin.php
@@ -1,65 +1,148 @@
 <?php
-@include 'config.php';
+    // Inicie a sessão
+    session_start();
+    @include 'config.php';
 
-if (isset($_POST['add_product'])) {
-    $p_name = $_POST['p_name'];
-    $p_price = $_POST['p_price'];
-    $p_image = $_FILES['p_image']['name'];
-    $p_image_tmp_name = $_FILES['p_image']['tmp_name'];
-    $p_image_folder = 'uploaded_img/' . $p_image;
+    // Inclua o arquivo de conexão com o banco de dados
+    require_once '../Login/conexaoBD.php';
 
-    $insert_query = $conn->prepare("INSERT INTO `products`(name, price, image) VALUES(:name, :price, :image)");
-    $insert_query->bindParam(':name', $p_name);
-    $insert_query->bindParam(':price', $p_price);
-    $insert_query->bindParam(':image', $p_image);
+    // Constante para o tamanho máximo do arquivo de imagem
+    define('TAMANHO_MAXIMO', (2 * 1024 * 1024));
 
-    if ($insert_query->execute()) {
-        move_uploaded_file($p_image_tmp_name, $p_image_folder);
-        $message[] = 'product added successfully';
-    } else {
-        $message[] = 'could not add the product';
+    // Verifique se o formulário foi enviado
+    if ($_SERVER["REQUEST_METHOD"] === 'POST') {
+
+        // Use suas funções de CRUD para verificar se o nome de usuário e a senha estão corretos
+
+        $nome = $_POST['nome'];
+        $preco = $_POST['preco'];
+        $descricao = $_POST['descricao'];
+        $quantidade = $_POST['quantidade'];
+        $tag = $_POST['tag'];
+
+        // Diretório de upload
+        $uploaddir = '../upload/'; //diretório onde será gravado a imagem
+
+        // Imagem
+        $imagem = $_FILES['p_image'];
+        $nomeImagem = $imagem['name'];
+        $tipoImagem = $imagem['type'];
+        $tamanhoImagem = $imagem['size'];
+
+        // Gerando novo nome para a imagem para evitar sobrescrita
+        $info = new SplFileInfo($nomeImagem);
+        $extensaoArq = $info->getExtension();
+        $novoNomeImagem = $nome . "." . $extensaoArq;
+
+        try {
+            if ((trim($nome) == "")) {
+                echo "<span id='warning'>Nome é obrigatório!</span>";
+
+            } else if ( ($nomeImagem != "") && (!preg_match('/^image\/(jpeg|png|gif)$/', $tipoImagem)) ) { //validção tipo arquivo
+                echo "<span id='error'>Isso não é uma imagem válida</span>";
+
+            } else if ( ($nomeImagem != "") && ($tamanhoImagem > TAMANHO_MAXIMO) ) { //validação tamanho arquivo
+                echo "<span id='error'>A imagem deve possuir no máximo 2 MB</span>";
+
+            } else {
+                if (
+                    ($nomeImagem != "") && 
+                    (move_uploaded_file($_FILES['p_image']['tmp_name'], 
+                                        $uploaddir . $novoNomeImagem)
+                    )
+                ) {
+                    // caminho/nome da imagem p/ gravar no BD
+                    $uploadfile = $uploaddir . $novoNomeImagem; 
+                } else {
+                    $uploadfile = null;
+                    echo "Sem upload de imagem.";
+                }
+
+                $insert_query = $pdo->prepare("INSERT INTO `produto`(nome, precoVenda, descricao, quantidade, tag, imagem) VALUES(:nome, :preco, :descricao, :quantidade, :tag, :imagem)");
+                $insert_query->bindParam(':nome', $nome);
+                $insert_query->bindParam(':preco', $preco);
+                $insert_query->bindParam(':descricao', $descricao);
+                $insert_query->bindParam(':quantidade', $quantidade);
+                $insert_query->bindParam(':tag', $tag);
+                $insert_query->bindParam(':imagem', $uploadfile);
+
+                if ($insert_query->execute()) {
+                    echo "Produto adicionado com sucesso";
+                } else {
+                    echo "Não foi possível adicionar o produto";
+                }
+            }
+        } catch(PDOException $e) {
+            echo 'Error: ' . $e->getMessage();
+        }
     }
-}
 
-if (isset($_GET['delete'])) {
-    $delete_id = $_GET['delete'];
-
-    $delete_query = $conn->prepare("DELETE FROM `products` WHERE id = :id");
-    $delete_query->bindParam(':id', $delete_id);
-
-    if ($delete_query->execute()) {
-        header('location:admin.php');
-        $message[] = 'product has been deleted';
-    } else {
-        header('location:admin.php');
-        $message[] = 'product could not be deleted';
+    if (isset($_GET['delete'])) {
+        $delete_id = $_GET['delete'];
+        $delete_query = $pdo->prepare("DELETE FROM `produto` WHERE id = :id");
+        $delete_query->bindParam(':id', $delete_id);
+        $delete_query->execute();
+        header("Location: admin.php");
     }
-}
 
-if (isset($_POST['update_product'])) {
-    $update_p_id = $_POST['update_p_id'];
-    $update_p_name = $_POST['update_p_name'];
-    $update_p_price = $_POST['update_p_price'];
-    $update_p_image = $_FILES['update_p_image']['name'];
-    $update_p_image_tmp_name = $_FILES['update_p_image']['tmp_name'];
-    $update_p_image_folder = 'uploaded_img/' . $update_p_image;
+    // Verifique se o produto está sendo atualizado
+    if (isset($_POST['update_product'])) {
+        $update_id = $_POST['update_p_id'];
+        $update_nome = $_POST['update_p_name'];
+        $update_preco = $_POST['update_p_price'];
 
-    $update_query = $conn->prepare("UPDATE `products` SET name = :name, price = :price, image = :image WHERE id = :id");
-    $update_query->bindParam(':name', $update_p_name);
-    $update_query->bindParam(':price', $update_p_price);
-    $update_query->bindParam(':image', $update_p_image);
-    $update_query->bindParam(':id', $update_p_id);
+        // Imagem
+        $imagem = $_FILES['update_p_image'];
+        $nomeImagem = $imagem['name'];
+        $tipoImagem = $imagem['type'];
+        $tamanhoImagem = $imagem['size'];
 
-    if ($update_query->execute()) {
-        move_uploaded_file($update_p_image_tmp_name, $update_p_image_folder);
-        $message[] = 'product updated successfully';
-        header('location:admin.php');
-    } else {
-        $message[] = 'product could not be updated';
-        header('location:admin.php');
+        // Gerando novo nome para a imagem para evitar sobrescrita
+        $info = new SplFileInfo($nomeImagem);
+        $extensaoArq = $info->getExtension();
+        $novoNomeImagem = $update_nome . "." . $extensaoArq;
+
+        // Diretório de upload
+        $uploaddir = '../upload/'; //diretório onde será gravado a imagem
+
+        if ($nomeImagem != "") {
+            if (!preg_match('/^image\/(jpeg|png|gif)$/', $tipoImagem)) { //validção tipo arquivo
+                echo "<span id='error'>Isso não é uma imagem válida</span>";
+                return;
+            } else if ($tamanhoImagem > TAMANHO_MAXIMO) { //validação tamanho arquivo
+                echo "<span id='error'>A imagem deve possuir no máximo 2 MB</span>";
+                return;
+            } else if (move_uploaded_file($_FILES['update_p_image']['tmp_name'], $uploaddir . $novoNomeImagem)) {
+                // caminho/nome da imagem p/ gravar no BD
+                $uploadfile = $uploaddir . $novoNomeImagem; 
+            } else {
+                echo "Falha no upload de imagem.";
+                return;
+            }
+        } else {
+            // Se nenhuma nova imagem for fornecida, mantenha a imagem original
+            $select_query = $pdo->prepare("SELECT imagem FROM `produto` WHERE id = :id");
+            $select_query->bindParam(':id', $update_id);
+            $select_query->execute();
+            $row = $select_query->fetch(PDO::FETCH_ASSOC);
+            $uploadfile = $row['imagem'];
+        }
+
+        $update_query = $pdo->prepare("UPDATE `produto` SET nome = :nome, precoVenda = :preco, imagem = :imagem WHERE id = :id");
+        $update_query->bindParam(':nome', $update_nome);
+        $update_query->bindParam(':preco', $update_preco);
+        $update_query->bindParam(':imagem', $uploadfile);
+        $update_query->bindParam(':id', $update_id);
+
+        if ($update_query->execute()) {
+            echo "Produto atualizado com sucesso";
+        } else {
+            echo "Não foi possível atualizar o produto";
+        }
     }
-}
 ?>
+
+
 
 
 <!DOCTYPE html>
@@ -96,53 +179,59 @@ if (isset($message)) {
     <section>
 
         <form action="" method="post" class="add-product-form" enctype="multipart/form-data">
-            <h3>add novo produto</h3>
-            <input type="text" name="p_name" placeholder="nome do produto" class="box" required>
-            <input type="number" name="p_price" min="0" placeholder="preço do produto" class="box" required>
+            <h3>Adicionar novo produto</h3>
+            <input type="text" name="nome" placeholder="nome do produto" class="box" required>
+            <input type="number" name="preco" min="0" placeholder="preço do produto" class="box" required>
+            <input type="text" name="descricao" min="0" placeholder="descrição" class="box" required>
+            <input type="number" name="quantidade" placeholder="quantidade" class="box" required>
+            <input type="text" name="tag" placeholder="tag" class="box">
             <input type="file" name="p_image" accept="image/png, image/jpg, image/jpeg" class="box" required>
             <input type="submit" value="salvar" name="add_product" class="btn">
+
         </form>
 
     </section>
 
     <section class="display-product-table">
 
-        <table>
 
-            <thead>
-                <th>imagem do produto</th>
-                <th>nome do produto</th>
-                <th>preço do produto</th>
-                <th>updates</th>
-            </thead>
+<table>
 
-            <tbody>
-                <?php
-                $select_products = $conn->query("SELECT * FROM `products`");
-                if ($select_products->rowCount() > 0) {
-                    while ($row = $select_products->fetch(PDO::FETCH_ASSOC)) {
-                ?>
+    <thead>
+        <th>imagem do produto</th>
+        <th>nome do produto</th>
+        <th>preço do produto</th>
+        <th>updates</th>
+    </thead>
 
-                        <tr>
-                            <td><img src="uploaded_img/<?php echo $row['image']; ?>" height="100" alt=""></td>
-                            <td><?php echo $row['name']; ?></td>
-                            <td>$<?php echo $row['price']; ?>/-</td>
-                            <td>
-                                <a href="admin.php?delete=<?php echo $row['id']; ?>" class="delete-btn" onclick="return confirm('are your sure you want to delete this?');"> <i class="fas fa-trash"></i> delete </a>
-                                <a href="admin.php?edit=<?php echo $row['id']; ?>" class="option-btn"> <i class="fas fa-edit"></i> update </a>
-                            </td>
-                        </tr>
+    <tbody>
+        <?php
+        $select_products = $pdo->query("SELECT * FROM `produto`");
+        if ($select_products->rowCount() > 0) {
+            while ($row = $select_products->fetch(PDO::FETCH_ASSOC)) {
+        ?>
 
-                <?php
-                    }
-                } else {
-                    echo "<div class='empty'>no product added</div>";
-                }
-                ?>
-            </tbody>
-        </table>
+                <tr>
+                    <td><img src="uploaded_img/<?php echo $row['imagem']; ?>" height="100" alt=""></td>
+                    <td><?php echo $row['nome']; ?></td>
+                    <td>R$<?php echo $row['precoVenda']; ?>/-</td>
+                    <td>
+                        <a href="admin.php?delete=<?php echo $row['id']; ?>" class="delete-btn" onclick="return confirm('are your sure you want to delete this?');"> <i class="fas fa-trash"></i> Deletar </a>
+                        <a href="admin.php?edit=<?php echo $row['id']; ?>" class="option-btn"> <i class="fas fa-edit"></i> Editar </a>
+                    </td>
+                </tr>
 
-    </section>
+        <?php
+            }
+        } else {
+            echo "<div class='empty'>no product added</div>";
+        }
+        ?>
+    </tbody>
+</table>
+
+</section>
+
 
     <section class="edit-form-container">
 
