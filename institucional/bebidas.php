23c49cd4d14db585f7ef0c56f91df874dfcf0cee
Aline Soares <99834657+n4rin@users.noreply.github.com>
2023-11-06 16:32:06 -0300

    adicionando bebidas

---
+++ institucional/bebidas.php
@@ -0,0 +1,203 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="icon" type="image/x-icon" href="../imagem/logo.PNG">
+    <link rel="stylesheet" href="../Produtos/produto.css">
+    <link rel="stylesheet" href="../Pagina/style.css">
+    <title>LivaTy - Drinks</title>
+</head>
+
+<body style="background-color: #f7f3f2; color: #27211E;">
+    <!--Inicio NavBar-->
+    <?php include '../Produtos/navbar2.php'; ?>
+
+    <br>
+    <div style="height: 100px;"></div>
+    <div id="achubaba" class="container border border-secondary-subtle row"
+        style="  display: flex; width: 980px;margin: auto; border-radius: 20px;"><br>
+        <div id="achubaba">
+            <section>
+                <div style="width: 500px;">
+                    <img src="https://s2.glbimg.com/MxgI9uDUDpnHZ6Dh_knf3TmNpnk=/620x455/e.glbimg.com/og/ed/f/original/2021/09/09/receita-drinque-nirvana-gin-suco-manga-curry-paprica-laranja-bebida-na-porta.png"
+                        alt="cafe" style="width: 930px; height: 500px; border-radius: 10px;">
+                </div><br>
+                <div style="width: 100%;">
+                    <div>
+                        <h4>Gin com chá verde e manga</h4>
+                    </div>
+                    <h4>Ingredientes:</h4>
+                    <li>1 colher (chá) de chá verde</li>
+                    <li>200 ml de água</li>
+                    <li>2 doses de gin</li>
+                    <li>5 fatias médias de manga</li>
+                    <li>5 folhas de hortelã frescas</li>
+                    <li>Gelo a gosto</li>
+                    <br>
+                    <h4>Modo de Preparo:</h4>
+                    <li>Esquente a água até uma temperatura entre 70 ºC e 80 ºC, ou seja, até que o fundo da chaleira
+                        apresente pequenas bolhas, e coloque o chá verde para infusionar pelo período de três a quatro
+                        minutos.</li>
+                    <li>Enquanto infusiona, adicione o gin, a manga, as folhas de hortelã e macere levemente, para que
+                        os sabores incorporem. Em seguida, coloque o gelo e coe.</li>
+                    <li>Misture bem e sirva em uma taça Copa de Balon, especialmente fabricada para bebidas a base de
+                        gin. Decore com folhas de hortelã e pedacinhos de manga.</li>
+                </div><br>
+        </div>
+        </section>
+    </div>
+
+    <div style="height: 50px;"></div>
+    <div id="achubaba" class="container border border-secondary-subtle row"
+        style="  display: flex; width: 980px;margin: auto; border-radius: 20px;"><br>
+        <div id="achubaba">
+            <section>
+                <div style="width: 500px;">
+                    <img src="https://vemat.com.br/blog/wp-content/uploads/2021/01/Mojito-com-ch%C3%A1-branco-1024x683.jpg"
+                        alt="cafe" style="width: 930px; height: 500px; border-radius: 10px;">
+                </div><br>
+                <div style="width: 100%;">
+                    <div>
+                        <h4>Mojito com chá branco</h4>
+                    </div>
+                    <h4>Ingredientes:</h4>
+                    <li>1 colher (chá) de chá branco</li>
+                    <li>200 ml de água</li>
+                    <li>1 dose de rum</li>
+                    <li>2 limões</li>
+                    <li>6 folhas de hortelã frescas</li>
+                    <li>Gelo a gosto</li>
+                    <br>
+                    <h4>Modo de Preparo:</h4>
+                    <li>O primeiro passo para realizar qualquer drink com chá é preparar o blend. Por isso, aqueça a
+                        água até a temperatura entre 70 ºC e 80 ºC (quando pequenas bolhas surgem na parte interior da
+                        chaleira) e desligue o fogo.</li>
+                    <li>Insira o chá branco e deixe infusionar por entre três e cinco minutos. Enquanto isso, na
+                        coqueteleira, coloque bastante gelo, o rum, o suco dos limões e as folhas de hortelã. Coe o
+                        blend no utensílio e agite bem.</li>
+                    <li>Sirva bem gelado — se necessário, adicione mais gelo, pois ele derreterá no choque térmico com o
+                        chá quente —. Decorar o copo com algumas rodelas de limão torna a apresentação ainda mais linda!
+                    </li>
+                </div><br>
+        </div>
+        </section>
+    </div>
+
+    <div style="height: 50px;"></div>
+    <div id="achubaba" class="container border border-secondary-subtle row"
+        style="  display: flex; width: 980px;margin: auto; border-radius: 20px;"><br>
+        <div id="achubaba">
+            <section>
+                <div style="width: 500px;">
+                    <img src="https://i.pinimg.com/736x/b1/23/82/b12382de4f37f3a6ef66669583537a8f.jpg" alt="cafe"
+                        style="width: 930px; height: 500px; border-radius: 10px;">
+                </div><br>
+                <div style="width: 100%;">
+                    <div>
+                        <h4>Caipirinha com chá verde</h4>
+                    </div>
+                    <h4>Ingredientes:</h4>
+                    <li>1 colher (chá) de chá verde</li>
+                    <li>1 colher (sopa) de açúcar</li>
+                    <li>200 ml de água</li>
+                    <li>2 e ½ doses de cachaça</li>
+                    <li>2 limões</li>
+                    <li>Gelo a gosto</li>
+                    <br>
+                    <h4>Modo de Preparo:</h4>
+                    <li>Novamente, o primeiro passo é preparar o chá, aquecendo a água até uma temperatura entre 70 ºC e
+                        80 ºC — no caso, o fogo pode ser desligado quando bolhas pequenas surgirem no fundo da chaleira.
+                    </li>
+                    <li>Adicione o chá verde e deixe infusionar por entre três a quatro minutos. Enquanto isso, adicione
+                        a cachaça, o suco dos limões e o açúcar na coqueteleira. Macere os ingredientes.</li>
+                    <li>Insira gelo no utensílio, coe o chá e misture tudo. Sirva com mais gelo em um copo decorado com
+                        rodelas de limão.</li>
+                </div><br>
+        </div>
+        </section>
+    </div>
+
+    <div style="height: 50px;"></div>
+    <div id="achubaba" class="container border border-secondary-subtle row"
+        style="  display: flex; width: 980px;margin: auto; border-radius: 20px;"><br>
+        <div id="achubaba">
+            <section>
+                <div style="width: 500px;">
+                    <img src="https://territoriosecreto.com.br/wp-content/uploads/2021/01/Sangria-de-vinho-tintos-e-a-receita-para-a-produzir-a-melhor.jpg" alt="cafe"
+                        style="width: 930px; height: 500px; border-radius: 10px;">
+                </div><br>
+                <div style="width: 100%;">
+                    <div>
+                        <h4>Sangria de chá</h4>
+                    </div>
+                    <h4>Ingredientes:</h4>
+                    <li>200 ml de água</li>
+                    <li>1 colher (chá) de hibisco seco</li>
+                    <li>½ colher (chá) de gengibre ralado</li>
+                    <li>1 dose de gin</li>
+                    <li>2 morangos picados</li>
+                    <li>1 cereja</li>
+                    <li>5 cubinhos pequenos de maçã</li>
+                    <li>1 laranja</li>
+                    <li>Água tônica para completar</li>
+                    <li>Gelo a gosto</li>
+                    <br>
+                    <h4>Modo de Preparo:</h4>
+                    <li>Esquente a água em uma chaleira até que ela atinja entre 90 ºC e 100 ºC — quando bolhas grandes
+                        surgirem na superfície do líquido —, desligue o fogo e coloque o hibisco em infusão.</li>
+                    <li>Enquanto isso, em uma coqueteleira, coloque o gengibre, o gin, os morangos, a cereja, a maçã, o
+                        suco da laranja e macere bem todos os ingredientes. Insira o gelo, coe o chá de hibisco e
+                        misture tudo por alguns minutos, para que os sabores incorporem.</li>
+                    <li>Adicione a bebida em um copo, complete com água tônica e, se preciso, coloque mais gelo. Para
+                        decorar, a dica é usar pedaços de laranja em formato de fatia de pizza na borda.</li>
+
+                </div><br>
+            </section>
+        </div>
+    </div>
+
+    <div style="height: 50px;"></div>
+    <div id="achubaba" class="container border border-secondary-subtle row"
+        style="  display: flex; width: 980px;margin: auto; border-radius: 20px;"><br>
+        <div id="achubaba">
+            <section>
+                <div style="width: 500px;">
+                    <img src="https://recipesblob.blob.core.windows.net/assets/d6c1385336e847dc9097abcfa8ae82ea/636x382/gintonicacomgelodecha.jpg"
+                        alt="cafe" style="width: 930px; height: 500px; border-radius: 10px;">
+                </div><br>
+                <div style="width: 100%;">
+                    <div>
+                        <h4>Gin com chá de hibisco</h4>
+                    </div>
+                    <h4>Ingredientes:</h4>
+                    <li>100 ml de água</li>
+                    <li>1 colher (chá) de hibisco seco</li>
+                    <li>1 colher (sopa) rasa açúcar</li>
+                    <li>1 dose de gin</li>
+                    <li>1 limão sicialiano</li>
+                    <li>Água com gás para completa</li>
+                    <li>Gelo a gosto</li>
+                    <br>
+                    <h4>Modo de Preparo:</h4>
+                    <li>Leve a água ao fogo, em uma chaleira, e esquente-a até uma temperatura entre 90 ºC e 100 ºC — quando bolhas grandes surgirem na superfície do utensílio. Desligue o fogo, adicione o hibisco seco e deixe em infusão por entre cinco e dez minutos.</li>
+                    <li>Enquanto isso, em uma coqueteleira, adicione o gin, o suco de limão siciliano (é possível colocar, também, algumas rodelas da fruta) e o açúcar. Macere bem, insira gelo no item e coe o chá.</li>
+                    <li>Chacoalhe a coqueteleira para que os sabores incorporem, despeje o drink em um copo longo e finalize com água com gás. Sirva em seguida.</li>
+                </div><br>
+            </section>
+        </div>
+    </div>
+
+    <!--Footer-->
+    <?php include '../nav/footer.php'; ?>
+
+    <script src="../Pagina/styles.js"></script>
+    <script src="../Pagina/carrinho.js"></script>
+</body>
+
+</html>
