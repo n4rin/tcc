05375c0a1a43974b5aa02eba2fb4c24e09ec533c
Vinícius Miguel <99834882+vinicius-magi@users.noreply.github.com>
2023-11-07 09:38:18 -0300

    header att

--- administracao/header.php
+++ administracao/header.php
@@ -5,8 +5,7 @@
       <a href="#" class="logo">Administração</a>
 
       <nav class="navbar">
-         <a href="admin.php">add produtos</a>
-         <a href="products.php">ver produtos</a>
+
       </nav>
 
       <?php
@@ -18,8 +17,6 @@
       }
       ?>
 
-
-      <a href="cart.php" class="cart">carrinho <span><?php echo $row_count; ?></span> </a>
 
       <div id="menu-btn" class="fas fa-bars"></div>
 
