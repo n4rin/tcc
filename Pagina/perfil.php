bbaeda1dfb4a00293cbd6a96a9ccb4e73913fa2b
Vinícius Miguel <99834882+vinicius-magi@users.noreply.github.com>
2023-11-07 10:13:48 -0300

    perfil

---
+++ Pagina/perfil.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        body {
+            background: rgb(0, 0, 0);
+    background: radial-gradient(circle, rgba(0, 0, 0, 1) 0%, rgba(0, 62, 28, 1) 0%, rgba(0, 0, 0, 1) 100%);
+            color: white;
+            font-family: Arial, sans-serif;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+        .profile {
+            width: 300px;
+            background-color: #008000;
+            border-radius: 10px;
+            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
+            text-align: center;
+            padding: 20px;
+        }
+        .profile img {
+            width: 100px;
+            height: 100px;
+            border-radius: 50%;
+            border: 3px solid white;
+            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
+        }
+        .profile h2, .profile p {
+            margin: 0;
+            padding: 10px 0;
+        }
+    </style>
+</head>
+<body>
+    <div class="profile">
+        <img src="perfil.jpg" alt="Foto do perfil">
+        <h2>Nome do Usuário</h2>
+        <p>@usuário</p>
+    </div>
+    <script>
+        // Adicione seu JavaScript aqui
+    </script>
+</body>
+</html>
