169e92a5ea42a64d55cf53a8dee98b076229828e
Aline Soares <99834657+n4rin@users.noreply.github.com>
2023-11-07 00:08:22 -0300

    add politica de priv

---
+++ institucional/privacidade.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="icon" type="image/x-icon" href="../imagem/logo.PNG">
+    <link rel="stylesheet" href="../Pagina/style.css">
+    <title>LivaTy - Saúde</title>
+</head>
+
+<body>
+    <!--Inicio NavBar-->
+    <?php include '../Produtos/navbar2.php'; ?>
+
+    <div style="height: 100px;"></div>
+    <h4 style="font-size: 20px; font-weight: 600; padding: 20px; color: white">Política de Privacidade de Dados de
+        Usuários</h4>
+    <h6 style="font-size: 20px; font-weight: 500; padding: 20px; color: white">Atualizado em: 06/11/2023</h6>
+    <h6 style="font-size: 20px; font-weight: 400; padding: 20px; color: white">Nós do LivaTY
+        valorizamos a privacidade e a segurança dos dados dos nossos clientes. Esta Política de Privacidade tem como
+        objetivo explicar como coletamos, usamos, compartilhamos e protegemos as informações pessoais dos usuários da
+        nossa loja virtual. Ao utilizar os nossos serviços, você concorda com as práticas descritas nesta política.</h6>
+
+    <ul style="font-size: 20px; font-weight: 500; padding: 20px; color: white">Informações Coletadas</ul>
+    <h6 style="font-size: 20px; font-weight: 400; padding: 20px; color: white">
+        1.1. Informações de Cadastro: Coletamos informações pessoais fornecidas por você ao se registrar na nossa loja,
+        incluindo nome, endereço de e-mail, número de cpf, número de telefone e outras informações de contato. <br>
+
+        1.2. Informações de Compra: Registramos os detalhes das suas compras, incluindo os produtos adquiridos, preços,
+        datas, métodos de pagamento e histórico de pedidos.
+
+    </h6>
+
+    <ul style="font-size: 20px; font-weight: 500; padding: 20px; color: white">Uso de Informações</ul>
+    <h6 style="font-size: 20px; font-weight: 400; padding: 20px; color: white">
+        2.1. As informações coletadas são utilizadas para:
+    </h6>
+    <div style="color: white; font-size: 20px; font-weight: 400; padding: 22px;">
+    <li>Processar pedidos e pagamentos;</li>
+    <li>Entregar produtos ao endereço de entrega fornecido;</li>
+    <li>Fornecer suporte ao cliente e responder a consultas;</li>
+    <li>Personalizar sua experiência de compra;</li>
+    <li>Melhorar nosso site e serviços;</li>
+    <li>Enviar comunicações de marketing, se você optar por recebê-las.</li>
+    </div>
+
+
+    <ul style="font-size: 20px; font-weight: 500; padding: 20px; color: white">Compartilhamento de Informações</ul>
+    <h6 style="font-size: 20px; font-weight: 400; padding: 20px; color: white">
+        3.1. Não vendemos, alugamos ou negociamos suas informações pessoais com terceiros para fins de marketing sem o
+        seu
+        consentimento expresso.
+        <br>
+        3.2. Podemos compartilhar suas informações com terceiros que prestam serviços essenciais, como processamento de
+        pagamento, entrega de produtos e atendimento ao cliente, desde que garantam a confidencialidade e segurança das
+        informações.
+        <br>
+        3.3. Em casos excepcionais, podemos divulgar informações pessoais em resposta a solicitações legais ou para
+        proteger
+        os direitos, propriedades e segurança da nossa loja, nossos clientes ou outras partes.
+    </h6>
+
+
+    <ul style="font-size: 20px; font-weight: 500; padding: 20px; color: white">Segurança de Dados</ul>
+    <h6 style="font-size: 20px; font-weight: 400; padding: 20px; color: white">
+        4.1. Implementamos medidas de segurança para proteger suas informações pessoais contra acesso não autorizado,
+        divulgação, alteração ou destruição.
+        <br>
+        4.2. No entanto, nenhum sistema é totalmente à prova de falhas, e a segurança da internet não pode ser
+        garantida.
+        Portanto, não podemos garantir a segurança das informações que você compartilha conosco.
+    </h6>
+
+
+    <ul style="font-size: 20px; font-weight: 500; padding: 20px; color: white">Seus Direitos</ul>
+    <h6 style="font-size: 20px; font-weight: 400; padding: 20px; color: white">
+        5.1. Você tem o direito de acessar, corrigir, atualizar ou excluir suas informações pessoais. Para exercer esses
+        direitos, entre em contato conosco.
+    </h6>
+
+
+    <ul style="font-size: 20px; font-weight: 500; padding: 20px; color: white">Consentimento e Alterações</ul>
+    <h6 style="font-size: 20px; font-weight: 400; padding: 20px; color: white">
+        6.1. Ao usar nosso site, você concorda com a coleta e uso das suas informações conforme descrito nesta política.
+        <br>
+        6.2. Reservamo-nos o direito de alterar esta Política de Privacidade a qualquer momento. Quaisquer alterações
+        serão
+        publicadas nesta página.
+    </h6>
+
+    <h6 style="font-size: 20px; font-weight: 400; padding: 20px; color: white">
+        Para perguntas ou preocupações sobre esta Política de Privacidade ou o uso das suas informações pessoais, entre
+        em
+        contato conosco através dos meios de contato disponíveis no nosso site.
+        <br>
+        Agradecemos por escolher o LivaTy para suas necessidades de produtos eserviços. Estamos comprometidos em
+        proteger sua privacidade e garantir a segurança dos seus dados pessoais.
+    </h6>
+
+    <!--Footer-->
+    <?php include '../nav/footer.php'; ?>
+
+    <script src="../Pagina/styles.js"></script>
+    <script src="../Pagina/carrinho.js"></script>
+</body>
+
+</html>
